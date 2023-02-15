7cf0adf10166504098dc60201f52114357db9191
zhangxin <15911072440@163.com>
2023-02-15 18:26:13 +0800

    add aaaa

---
+++ usermgr-controller-xin/src/main/java/com/xin/branch/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.xin.branch;
+
+/**
+ * idea分支
+ * 2023/2/15
+ * 作者：张欣
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaa");
+    }
+}

83afe7f80163af954a684ba596d392e14135ebc1
zhangxin <15911072440@163.com>
2023-02-15 21:15:40 +0800

    add eeeeeee

--- usermgr-controller-xin/src/main/java/com/xin/branch/HelloWorld.java
+++ usermgr-controller-xin/src/main/java/com/xin/branch/HelloWorld.java
@@ -12,5 +12,6 @@ public class HelloWorld {
         System.out.println("mybranch add ccccccc");
         System.out.println("mybranch local add ddddddd");
         System.out.println("mybranch local add eeeeeeeeee");
+        System.out.println("mybranch local add ffffffffffffffffff");
     }
 }
